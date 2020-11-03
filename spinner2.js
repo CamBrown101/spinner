21e80ce8f58abf9cf3148195da8afc682e7dfb3a
Cameron Brown <cameron-brown@live.com>
2020-11-03 20:59:59 +0000

    refractored spinner1 into a function

--- spinner2.js
+++ spinner2.js
@@ -1,51 +1,12 @@
-// setTimeout(() => {
-//   process.stdout.write('\r |    ');
-// }, 100);
-// setTimeout(() => {
-//   process.stdout.write('\r /    ');
-// }, 300);
-// setTimeout(() => {
-//   process.stdout.write('\r -    ');
-// }, 500);
-// setTimeout(() => {
-//   process.stdout.write('\r \\    ');
-// }, 700);
-// setTimeout(() => {
-//   process.stdout.write('\r |    ');
-// }, 900);
-// setTimeout(() => {
-//   process.stdout.write('\r /    ');
-// }, 1100);
-// setTimeout(() => {
-//   process.stdout.write('\r -    ');
-// }, 1300);
-// setTimeout(() => {
-//   process.stdout.write('\r \\    ');
-// }, 1500);
-// setTimeout(() => {
-//   process.stdout.write('\r |    ');
-// }, 1700);
-
 const spinner = () => {
-  const line = ["|", "/", '-', '\\', '|', "/", '-', '\\', '|']
-  timeout = 100;
+  const line = ["|", "/", '-', '\\', '|', "/", '-', '\\', '|'];
+  let timeout = 100;
   for (let i = 0; i < line.length; i++) {
     setTimeout(() => {
       process.stdout.write(`\r ${line[i]}    `);
     }, timeout);
     timeout += 200;
   }
-}
+};
 
-spinner()
-
-// for (const char of sentence) {
-//   setTimeout(() => {
-//     process.stdout.write(char);
-//     printString += char;
-//     if (printString.length === sentence.length) {
-//       console.log('');
-//     }
-//   }, delay);
-//   delay += 50;
-// }
+spinner();
